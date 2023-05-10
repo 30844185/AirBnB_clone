b59419eac4e69a728713565e5f92090d3a203dd0
30844185 <danielhika05@gmail.com>
2023-05-10 10:42:06 -0700

    correction

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Module for User class."""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Class that defines attributes for an user """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
